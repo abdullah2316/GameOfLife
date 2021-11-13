d2784decfb9ef4eec7ed3502ef78c43862b7cedb
rattlesnakexd <93007896+rattlesnakexd@users.noreply.github.com>
2021-11-13 11:13:48 -0800

    Add files via upload
    Main for unit testing added

---
+++ BL/src/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        GameLogic object = new GameLogic();
+        object.ConstructBoard();
+        object.displayBoard();
+    }
+}
