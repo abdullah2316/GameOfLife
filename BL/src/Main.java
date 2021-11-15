f06070ea901c29c506d15151e70d89027986e25a
rattlesnakexd <93007896+rattlesnakexd@users.noreply.github.com>
2021-11-15 20:28:15 +0500

    Main for unit testing updated

--- BL/src/Main.java
+++ BL/src/Main.java
@@ -1,11 +1,30 @@
-
+package com.company;
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws InterruptedException {
+        // write your code here
+        int i = 0;
         GameLogic object = new GameLogic();
         object.ConstructBoard();
         object.displayBoard();
+        Thread.sleep(1000);
+        clearScreen();
+        while(i < 2) {
+
+            object.updateBoard();
+            System.out.println();
+            object.displayBoard();
+            Thread.sleep(1000);
+            i++;
+            clearScreen();
+        }
+
     }
+    public static void clearScreen() throws InterruptedException {
+        System.out.println(System.lineSeparator().repeat(100));
+    }
+
 }
+
