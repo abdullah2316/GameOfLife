3e26d43724464510de5f4229d13c2d1eac0f67ac
Abdullah Sheikh <80975445+abdullahh-1@users.noreply.github.com>
2021-11-17 22:59:27 +0500

    Update GameBoard.java
    Fixed Bugs

--- BL/src/GameBoard.java
+++ BL/src/GameBoard.java
@@ -1,5 +1,3 @@
-package com.company;
-
 public class GameBoard {
     private Cell[][] Cells;
     int Size;
@@ -7,24 +5,25 @@ public class GameBoard {
     public GameBoard(){
         Cells = new Cell[10][10];
         Size = 10;
-        for(int i=0; i < 10 ; i++){
-            for(int j=0; j < 10 ; j++){
+        for(int i=0; i < Size ; i++){
+            for(int j=0; j < Size ; j++){
                 Cells[i][j] = new Cell();
             }
         }
-        /*Cells[2][4].ChangeState();
+        Cells[2][4].ChangeState();
         Cells[3][4].ChangeState();
         Cells[4][4].ChangeState();
         Cells[3][5].ChangeState();
-        Cells[3][3].ChangeState();*/
-        
-        Cells[4][4].ChangeState();
-        Cells[4][3].ChangeState();
-        Cells[4][5].ChangeState();
+        Cells[3][3].ChangeState();
     }
     public GameBoard(int size){
         Size = size;
         Cells = new Cell[size][size];
+        for(int i=0; i < Size ; i++){
+            for(int j=0; j < Size ; j++){
+                Cells[i][j] = new Cell();
+            }
+        }
     }
 
     public void displayBoard(){
@@ -45,27 +44,39 @@ public class GameBoard {
     public boolean isAlive(int row, int column){
         return Cells[row][column].check_IsAlive();
     }
-    public void ResetBoard(){
 
+    public void ResetBoard(){
+        for(int i=0; i < Size ; i++){
+            for(int j=0; j < Size ; j++){
+                Cells[i][j].setAlive(false);
+            }
+        }
+        Cells[2][4].ChangeState();
+        Cells[3][4].ChangeState();
+        Cells[4][4].ChangeState();
+        Cells[3][5].ChangeState();
+        Cells[3][3].ChangeState();
     }
+
     public boolean getCell (int row, int column) {
         return Cells[row][column].check_IsAlive();
     }
-    public int Size (){
+
+    public int getSize (){
         return Size;
     }
     
     public int check_neighbor(int row, int col) {
         int count = 0;
         
-        if (row > 0)                            if (Cells[row - 1][col].check_IsAlive()) count++;
-        if (row > 0 && col > 0)                 if (Cells[row - 1][col - 1].check_IsAlive()) count++;
-        if (row > 0 && col < Size - 1)          if (Cells[row - 1][col + 1].check_IsAlive()) count++;
-        if (row < Size - 1)                     if (Cells[row + 1][col].check_IsAlive()) count++;
-        if (row < Size - 1 && col > 0)          if (Cells[row + 1][col - 1].check_IsAlive()) count++;
-        if (row < Size - 1 && col < Size - 1)   if (Cells[row + 1][col + 1].check_IsAlive()) count++;
-        if (row < Size - 1 && col > 0)   if (Cells[row][col - 1].check_IsAlive()) count++;
-        if (col < Size - 1)                     if (Cells[row][col + 1].check_IsAlive()) count++;
+        if (row > 0)                            if (Cells[row - 1]  [col]       .check_IsAlive()) count++;
+        if (row > 0 && col > 0)                 if (Cells[row - 1]  [col - 1]   .check_IsAlive()) count++;
+        if (row > 0 && col < Size - 1)          if (Cells[row - 1]  [col + 1]   .check_IsAlive()) count++;
+        if (row < Size - 1)                     if (Cells[row + 1]  [col]       .check_IsAlive()) count++;
+        if (row < Size - 1 && col > 0)          if (Cells[row + 1]  [col - 1]   .check_IsAlive()) count++;
+        if (row < Size - 1 && col < Size - 1)   if (Cells[row + 1]  [col + 1]   .check_IsAlive()) count++;
+        if (row < Size - 1 && col > 0)          if (Cells[row]      [col - 1]   .check_IsAlive()) count++;
+        if (col < Size - 1)                     if (Cells[row]      [col + 1]   .check_IsAlive()) count++;
 
         return count;
     }
