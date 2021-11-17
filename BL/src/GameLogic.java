2fec454b971f77210a7e48d9c483425190a836e5
Abdullah Sheikh <80975445+abdullahh-1@users.noreply.github.com>
2021-11-17 23:00:43 +0500

    Update GameLogic.java
    Added Reset and States

--- BL/src/GameLogic.java
+++ BL/src/GameLogic.java
@@ -1,8 +1,5 @@
-package com.company;
-
 public class GameLogic {
     private GameBoard Board;
-    private GameBoard NewBoard;
     private boolean State;
     private int Zoom_Factor;
     private int Counter;
@@ -14,10 +11,10 @@ public class GameLogic {
         Counter = 0;
         Speed = 1;
         SpeedLimit = 10;
+        State = true;
     }
     public void ConstructBoard(){
         Board = new GameBoard();
-        NewBoard = new GameBoard();
     }
     public void displayBoard(){
         Board.displayBoard();
@@ -30,21 +27,18 @@ public class GameLogic {
     public void updateBoard(){
         GameBoard temp = new GameBoard(Board.getSize());
         temp.copy(Board);
-
         int size = Board.getSize();
         for(int i = 0 ; i < size ; i++)
         {
             for(int j = 0 ; j < size ; j++)
             {
                 int Neighbor_Count = Board.check_neighbor(i, j);
+
                 if (( Neighbor_Count == 3) &&! Board.isAlive(i, j))
-                {
                     temp.isClicked(i,j);
-                }
+
                 else if(( Neighbor_Count < 2 || Neighbor_Count > 3) && Board.isAlive(i, j))
-                {
                     temp.isClicked(i,j);
-                }
             }
         }
         Board.copy(temp);
@@ -77,6 +71,6 @@ public class GameLogic {
         Zoom_Factor = 1;
         Counter = 0;
         Speed = 1;
-        //Board.ResetBoard();
+        Board.ResetBoard();
     }
 }
