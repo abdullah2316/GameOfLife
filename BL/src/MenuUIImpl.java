c91d0d47a4b17a26fe206a7a973a102a19be289a
Abdullah Sheikh <80975445+abdullahh-1@users.noreply.github.com>
2021-11-24 23:17:23 +0500

    MenuUIImpl.java Added

---
+++ BL/src/MenuUIImpl.java
@@ -0,0 +1,11 @@
+public class MenuUIImpl implements MenuUI {
+    @Override
+    public void LoadSavedStates(StringBuilder[] Date_Time, int[] Coordinates) {
+
+    }
+
+    @Override
+    public void DeleteState(int id) {
+
+    }
+}
