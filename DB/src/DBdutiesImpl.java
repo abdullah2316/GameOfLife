9708373b84439f3e8117857d30e73087c9a28184
Abdullah Sheikh <80975445+abdullahh-1@users.noreply.github.com>
2021-11-27 23:01:10 +0500

    added generations to save and VIEWSAVEDSTATES()

--- DB/src/DBdutiesImpl.java
+++ DB/src/DBdutiesImpl.java
@@ -1,5 +1,4 @@
 import org.jetbrains.annotations.NotNull;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.time.format.DateTimeFormatter;
@@ -22,12 +21,40 @@ public class DBdutiesImpl implements DBduties {
         }
     }
 
+    public StringBuilder[][] viewSavedStates() throws SQLException {
+        String query = "Select * from SaveInfo";
+        ResultSet m_ResultSet = m_Statement.executeQuery(query);
+        int count = getSaves();
+        StringBuilder[][] array = new StringBuilder[count][];
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < 5; j++) {
+                array[i][j] = new StringBuilder("");
+            }
+        }
+        int i = 0;
+        while (m_ResultSet.next()) {
+            array[i][0].append(m_ResultSet.getInt(1));
+            array[i][1].append(m_ResultSet.getString(2));
+            array[i][2].append(m_ResultSet.getString(3));
+            array[i][3].append(m_ResultSet.getString(4));
+            array[i][4].append(m_ResultSet.getString(5));
+                
+            i += 1;
+        }
+        return array;
+    }
+
+
+
+
     int getSaves() throws SQLException {
         String query = "SELECT MAX([Save]) from Cells";
         ResultSet m_ResultSet = m_Statement.executeQuery(query);
         m_ResultSet.next();
         return m_ResultSet.getInt(1);
     }
+
+
     int getCount(int Save) throws SQLException {
         String query = "SELECT Count(*) FROM Cells where [Save] = " + valueOf(Save);
         ResultSet m_ResultSet = m_Statement.executeQuery(query);
@@ -51,11 +78,12 @@ public class DBdutiesImpl implements DBduties {
         return array;
     }
 
-    public void save(String Name, @NotNull ArrayList<Integer> array) throws SQLException {
+
+    public void save(String Name, int Generation, @NotNull ArrayList<Integer> array) throws SQLException {
         String query = "SELECT Max([Save]) FROM Cells";
         ResultSet m_ResultSet = m_Statement.executeQuery(query);
         int Save;
-        if (m_ResultSet.next() == false)
+        if (!m_ResultSet.next())
             Save = 1;
         else
             Save = m_ResultSet.getInt(1) + 1;
@@ -63,7 +91,7 @@ public class DBdutiesImpl implements DBduties {
         for (int  i = 0 ; i < array.size() ; i += 2) {
             m_Statement.executeUpdate("INSERT INTO Cells VALUES (" + Save + ", " + valueOf(array.get(i)) + "," + valueOf(array.get(i + 1)) + ")");
         }
-        String query2 = "INSERT INTO SAVE_INFO VALUES ( " + Save + ", '" + Name + "' , NULL , NULL)";
+        String query2 = "INSERT INTO SAVE_INFO VALUES ( " + Save + ", '" + Name + "', " + Generation +", NULL , NULL)";
         m_Statement.executeUpdate(query2);
         // TIME FORMAT: 00:01:14.341,  DATE FORMAT: 2017-01-23
     }
