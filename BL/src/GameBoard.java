e8c952a9187d5051bf020f08440e606940ab8ce4
Abdullah Sheikh <80975445+abdullahh-1@users.noreply.github.com>
2021-11-28 20:38:55 +0500

    added row and col size

--- BL/src/GameBoard.java
+++ BL/src/GameBoard.java
@@ -3,30 +3,33 @@ import java.util.ArrayList;
 
 public class GameBoard {
     private Cell[][] Cells;
-    int Size;
+    int Row_Size;
+    int Col_Size;
 
     public GameBoard(){
-        Cells = new Cell[10][10];
-        Size = 10;
-        for(int i=0; i < Size ; i++) {
-            for (int j = 0; j < Size; j++) {
+        Row_Size = 40;
+        Col_Size = 100;
+        Cells = new Cell[Row_Size][Col_Size];
+        for(int i=0; i < Row_Size ; i++) {
+            for (int j = 0; j < Col_Size; j++) {
                 Cells[i][j] = new Cell();
             }
         }
     }
-    public GameBoard(int size){
-        Size = size;
-        Cells = new Cell[size][size];
-        for(int i=0; i < Size ; i++){
-            for(int j=0; j < Size ; j++){
+    public GameBoard(int row, int col){
+        Col_Size = col;
+        Row_Size = row;
+        Cells = new Cell[row][col];
+        for(int i=0; i < row ; i++){
+            for(int j=0; j < col ; j++){
                 Cells[i][j] = new Cell();
             }
         }
     }
 
     public void displayBoard(){
-        for(int i=0; i < 10 ; i++){
-            for(int j=0; j < 10 ; j++){
+        for(int i=0; i < Row_Size ; i++){
+            for(int j=0; j < Col_Size ; j++){
                 if (Cells[i][j].check_IsAlive())
                     System.out.print(" 0 ");
                 else
@@ -45,8 +48,8 @@ public class GameBoard {
 
     public ArrayList<Integer> ClearBoard() {
         ArrayList<Integer> array = new ArrayList<Integer>();
-        for (int i = 0; i < Size; i++) {
-            for (int j = 0; j < Size; j++) {
+        for (int i = 0; i < Row_Size; i++) {
+            for (int j = 0; j < Col_Size; j++) {
                 if(Cells[i][j].check_IsAlive()) {
                     Cells[i][j].setAlive(false);
                     array.add(i);
@@ -86,21 +89,25 @@ public class GameBoard {
         return Cells[row][column].check_IsAlive();
     }
 
-    public int getSize (){
-        return Size;
+    public int getRowSize (){
+        return Row_Size;
+    }
+
+    public int getColSize (){
+        return Col_Size;
     }
     
     public int check_neighbor(int row, int col) {
         int count = 0;
 
-        if (row > 0)                            if (Cells[row - 1][col].check_IsAlive())        count++;
-        if (row > 0 && col > 0)                 if (Cells[row - 1][col - 1].check_IsAlive())    count++;
-        if (row > 0 && col < Size - 1)          if (Cells[row - 1][col + 1].check_IsAlive())    count++;
-        if (row < Size - 1)                     if (Cells[row + 1][col].check_IsAlive())        count++;
-        if (row < Size - 1 && col > 0)          if (Cells[row + 1][col - 1].check_IsAlive())    count++;
-        if (row < Size - 1 && col < Size - 1)   if (Cells[row + 1][col + 1].check_IsAlive())    count++;
-        if (row < Size - 1 && col > 0)          if (Cells[row][col - 1].check_IsAlive())        count++;
-        if (col < Size - 1)                     if (Cells[row][col + 1].check_IsAlive())        count++;
+        if (row > 0)                                    if (Cells[row - 1][col].check_IsAlive())        count++;
+        if (row > 0 && col > 0)                         if (Cells[row - 1][col - 1].check_IsAlive())    count++;
+        if (row > 0 && col < Col_Size - 1)              if (Cells[row - 1][col + 1].check_IsAlive())    count++;
+        if (row < Row_Size - 1)                         if (Cells[row + 1][col].check_IsAlive())        count++;
+        if (row < Row_Size - 1 && col > 0)              if (Cells[row + 1][col - 1].check_IsAlive())    count++;
+        if (row < Row_Size - 1 && col < Col_Size - 1)   if (Cells[row + 1][col + 1].check_IsAlive())    count++;
+        if (row < Row_Size - 1 && col > 0)              if (Cells[row][col - 1].check_IsAlive())        count++;
+        if (col < Col_Size - 1)                         if (Cells[row][col + 1].check_IsAlive())        count++;
 
         return count;
     }
@@ -108,9 +115,9 @@ public class GameBoard {
 
     public void copy (GameBoard obj)
     {
-        for (int i=0; i< Size; i++)
+        for (int i=0; i < Row_Size; i++)
         {
-            for(int j=0; j< Size; j++)
+            for(int j=0; j < Col_Size; j++)
             {
                 Cells[i][j].setAlive(obj.Cells[i][j].check_IsAlive());
             }
@@ -119,9 +126,9 @@ public class GameBoard {
     public void copyfromArray (int[] array)
     {
         int index = 1;
-        for (int i=0; i< Size; i++)
+        for (int i=0; i< Row_Size; i++)
         {
-            for(int j=0; j< Size; j++)
+            for(int j=0; j< Col_Size; j++)
             {
                 if(index < array[0]) {
                     if (array[index] == i && array[index + 1] == j) {
@@ -138,8 +145,8 @@ public class GameBoard {
     }
     public ArrayList<Integer> copyToArray () {
         ArrayList<Integer> array = new ArrayList<Integer>();
-        for (int i = 0; i < Size; i++)
-            for (int j = 0; j < Size; j++)
+        for (int i = 0; i < Row_Size; i++)
+            for (int j = 0; j < Col_Size; j++)
                 if (Cells[i][j].check_IsAlive()) {
                     array.add(i);
                     array.add(j);
