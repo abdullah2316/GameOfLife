db7e52cca1b51e80909878177f462bf82e5a6ce3
abdullah naeem <72752489+abdullah2316@users.noreply.github.com>
2021-11-28 14:37:53 +0500

    Update DBduties.java

--- BL/src/DBduties.java
+++ BL/src/DBduties.java
@@ -2,6 +2,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface DBduties {
-    public int[] load(int Save) throws SQLException;
+   
     public void save(String Name, int Generation, ArrayList<Integer> array) throws SQLException;
+    public  void Load(ArrayList<StringBuilder> info, ArrayList<Integer> cells);
+    public  void deletestate(int id);
 }
