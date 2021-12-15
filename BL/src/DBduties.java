761a0be3edd8a1ecf69871e3b6e66a3b96b40e33
Padfoot <94177842+TheAbdullahNaeem@users.noreply.github.com>
2021-12-15 18:28:14 +0500

    Delete BL directory

--- BL/src/DBduties.java
+++
@@ -1,9 +0,0 @@
-import java.sql.SQLException;
-import java.util.ArrayList;
-
-public interface DBduties {
-   
-    public void save(String Name, int Generation, ArrayList<Integer> array);
-    public  void Load(ArrayList<StringBuilder> info, ArrayList<Integer> cells);
-    public  void deletestate(String id);
-}
--- BL/src/MenuUI.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public interface MenuUI {
-    void LoadSavedStates(ArrayList<StringBuilder> Date_Time, ArrayList<Integer> Coordinates);
-
-    void DeleteState(int id);
-
-    ArrayList<Integer> start();
-}
